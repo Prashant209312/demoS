a81cefaa9f4700d80418efd26fd7cf8a26fdd969
prashant <psachan@purecodesoftware.com>
2024-02-15 15:18:59 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+// import { useState } from "react";
+// import { Route, Routes } from "react-router-dom";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { useState } from "react";
+import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
-    <div>
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
