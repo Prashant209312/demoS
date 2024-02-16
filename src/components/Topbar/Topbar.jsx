3896af003a41dcdbdced57898bcbad455c917356
prashant <psachan@purecodesoftware.com>
2024-02-16 16:13:25 +0530

    contact code added

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,6 +1,5 @@
 import { Box, IconButton, useTheme, } from "@mui/material";
 import { useContext } from "react";
-
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -8,7 +7,7 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import { ColorModeContext, tokens } from "../theme";
+import { ColorModeContext, tokens } from "../../theme";
 
 const Topbar = () => {
     const theme = useTheme();
@@ -17,7 +16,6 @@ const Topbar = () => {
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}>
-            {/* SEARCH BAR */}
             <Box
                 display="flex"
                 backgroundColor={colors.primary[400]}
@@ -28,8 +26,6 @@ const Topbar = () => {
                     <SearchIcon />
                 </IconButton>
             </Box>
-
-            {/* ICONS */}
             <Box display="flex">
                 <IconButton onClick={colorMode.toggleColorMode}>
                     {theme.palette.mode === "dark" ? (
